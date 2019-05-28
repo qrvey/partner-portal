16f36e77b815f13f8e0dbe8ce573970e24783590
Wilson Manzano <wilson.manzano@qrvey.com>
2019-05-28 15:48:55 -0500

    Removed unused Homeplash Component

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -13,47 +13,22 @@ const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-class HomeSplash extends React.Component {
-  render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper" id="home-wrapper">
-          <TopFold/>
-          </div>
-        </div>
-      </div>
-    );
-
-    const TopFold = () => (
-      <div className="flex-container">
-      <div className="two-row">
-        <h2 className="main-title">
-        Begin your journey <br/>
-        with our Detailed <br/>
-        Documents
-        </h2>
-      </div>
-      <div className="two-row">
-        <img src={siteConfig.baseUrl + 'img/docs-top-fold.jpg'} alt="Project Logo"/>
-      </div>
-      </div>
-    );
-
-    return (
-      <SplashContainer>
-      </SplashContainer>
-    );
-  }
-}
-
 class Index extends React.Component {
+  
+
+  componentDidMount(){
+    this.insertItemToNav();
+  }
+
+  insertItemToNav(){
+    console.log('hola');
+    let navbar = document.querySelector('.slidingNav ul');
+    const newItem = document.createElement("LI");
+    const textnode = document.createTextNode("Training");
+    newItem.appendChild(textnode);
+    navbar.insertBefore(newItem, list.childNodes[0]);
+  }
+
   render() {
     const {config: siteConfig, language = ''} = this.props;
 
@@ -77,11 +52,37 @@ class Index extends React.Component {
         <h1 className="section-card-title"><img className="icon" src={siteConfig.baseUrl + 'img/categories/' + props.value.icon + '.svg'}/>{props.value.name}</h1>
       </a>
     );
+
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper" id="home-wrapper">
+          <TopFold/>
+          </div>
+        </div>
+      </div>
+    );
+
+    const TopFold = () => (
+      <div className="flex-container">
+      <div className="two-row">
+        <h2 className="main-title">
+        Begin your journey <br/>
+        with our Detailed <br/>
+        Documents
+        </h2>
+      </div>
+      <div className="two-row">
+        <img className="top-fold-image" src={siteConfig.baseUrl + 'img/docs-top-fold.jpg'} alt="Project Logo"/>
+      </div>
+      </div>
+    );
     
 
     return (
       <div className="gray-background" onClick={()=> alert("Hello! I am an alert box!!")}>
-        <HomeSplash siteConfig={siteConfig} language={language} />
+        <SplashContainer siteConfig={siteConfig} language={language} />
         <div className="mainContainer" id="main-container">
         <Categories/>
         </div>
