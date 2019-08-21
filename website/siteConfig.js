dd6b9394eebaffbc5d022a564371acfd30a536e6
Hender Ruiz <hender.ruiz@qrvey.com>
2019-08-21 09:49:52 -0500

    Dropdown scroll fixed

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,7 +40,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {
-      href: '#',
+      href: '',
       label: 'Documentation',
     },
     {
