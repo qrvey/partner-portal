0f122fb05811bd6b040923465babc254399b640a
Domingo Sanchez <>
2021-09-14 13:12:17 -0500

    Change version to 6.2

--- v2-website/src/pages/index.js
+++ v2-website/src/pages/index.js
@@ -102,7 +102,7 @@ class Index extends React.Component {
               <img src={siteConfig.baseUrl + 'img/icons/calendar_icon.svg'}/>
               <h3 className="side-right-nav-container-announcements-title"> Announcements</h3>
               <h3 className="side-right-nav-container-announcements-item"> 
-              Version 6.1 has been released, <a className="primary-color" href="docs/release-notes/release-last/">learn more.</a>
+              Version 6.2 has been released, <a className="primary-color" href="docs/release-notes/release-last/">learn more.</a>
                </h3>
           </div>
           <div className="side-right-nav-container-popular">
