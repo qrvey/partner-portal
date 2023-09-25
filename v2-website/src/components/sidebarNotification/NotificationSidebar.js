674452aed495f36c8546874c661db2339f1517d6
Domingo Sanchez <domingo.sanchez@qrvey.com>
2023-09-25 14:23:46 -0500

    update notifications component

--- v2-website/src/components/sidebarNotification/NotificationSidebar.js
+++ v2-website/src/components/sidebarNotification/NotificationSidebar.js
@@ -7,21 +7,30 @@ function NotificationSidebar() {
   // Ejemplo de array de notificaciones:
   const notifications = [
     {
+      title: "Customizing the Appearance Using CSS",
+      description: "This article provides guidelines for properly using CSS to customize the visual theme of embedded widgets.",
+      link: "/docs/software-developer/customizing-css-variables"
+    },
+    {
       title: "Download Manager",
       description: "Introduced in version 8.2, Download Manager provides a central location to access and manage exported files.",
-      link: "#"
+      link: "/docs/composer/Managing%20Your%20User%20Profile/download-manager"
     },
     {
-      title: "Formula Builder",
+      title: "Using Standardized Syntax",
       description: "Version 8.2 enables you to create formulas using a standardized language that works with any database engine supported by the Qrvey platform.",
-      link: "#"
+      link: "/docs/composer/Working%20with%20Data/Datasets/Analyze/Formula%20Builder/formula-builder-standardized-syntax"
     },
     {
-      title: "Advanced Settings",
-      description: "Provides advanced configuration options for your dataset, including the ability to set the number of shards allocated to the dataset’s ES index.",
-      link: "#"
+      title: "Introduction to the Qrvey Platform",
+      description: "The Qrvey platform is an all-in-one analytics solution that enables SaaS providers to put cutting-edge analytics into the hands of their users. This article contains essential information required to understand how to work with the Qrvey platform.",
+      link: "/docs/getting-started/intro-to-qrvey"
     },
   ];
+
+
+
+
 
   return (
     <div>
