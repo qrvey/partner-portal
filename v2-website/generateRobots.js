79b022fdc0b4bd8c9248b7a838a70b6e46faa915
Domingo Sanchez <domingo.sanchez@qrvey.com>
2023-10-20 12:46:43 -0500

    Update env configuration

--- v2-website/generateRobots.js
+++ v2-website/generateRobots.js
@@ -1,22 +1,36 @@
 const fs = require('fs');
 
 const {
+    ENVIRONMENT,
     BASE_URL_PROD,
     BASE_URL_STAGING,
     BASE_URL_DEV
 } = process.env;
 
+let BASE_URL;
+
+switch (ENVIRONMENT) {
+    case 'prod':
+        BASE_URL = BASE_URL_PROD;
+        break;
+    case 'staging':
+        BASE_URL = BASE_URL_STAGING;
+        break;
+    case 'dev':
+        BASE_URL = BASE_URL_DEV;
+        break;
+    default:
+        console.error('Ambiente no reconocido');
+        process.exit(1);
+}
+
 let content;
 
-
+// Verificar el ambiente actual basándose en la URL base
 if (BASE_URL === BASE_URL_PROD) {
-    content = 'User-agent: *\nAllow: /';  
-} else if (BASE_URL === BASE_URL_STAGING) {
-    content = 'User-agent: *\nDisallow: /';  
-} else if (BASE_URL === BASE_URL_DEV) {
-    content = 'User-agent: *\nDisallow: /';  
+    content = 'User-agent: *\nAllow: /';  // Por defecto, permite a todos los rastreadores
 } else {
-    content = 'User-agent: *\nAllow: /';
+    content = 'User-agent: *\nDisallow: /';  // Bloquea todos los rastreadores en staging y dev
 }
 
 fs.writeFileSync('static/robots.txt', content);
