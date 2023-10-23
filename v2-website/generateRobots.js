ac9fdf1e80db0e19d81fb77463bbd3db22b58c46
Domingo Sanchez <domingo.sanchez@qrvey.com>
2023-10-23 14:14:30 -0500

    edit script for robots

--- v2-website/generateRobots.js
+++ v2-website/generateRobots.js
@@ -1,41 +1,16 @@
 const fs = require('fs');
 
-const {
-    ENVIRONMENT,
-    BASE_URL_PROD,
-    BASE_URL_STAGING,
-    BASE_URL_DEV
-} = process.env;
-
-
-console.log('ENVIRONMENT:', ENVIRONMENT);
-console.log('BASE_URL_PROD:', BASE_URL_PROD);
-console.log('BASE_URL_STAGING:', BASE_URL_STAGING);
-console.log('BASE_URL_DEV:', BASE_URL_DEV);
-
-let BASE_URL;
-
-switch (ENVIRONMENT) {
-    case 'prod':
-        BASE_URL = BASE_URL_PROD;
-        break;
-    case 'staging':
-        BASE_URL = BASE_URL_STAGING;
-        break;
-    case 'dev':
-        BASE_URL = BASE_URL_DEV;
-        break;
-    default:
-        console.error('Ambiente no reconocido');
-        process.exit(1);
-}
+const { ENVIRONMENT } = process.env;
 
 let content;
 
-if (BASE_URL === BASE_URL_PROD) {
+if (ENVIRONMENT === 'prod') {
     content = 'User-agent: *\nAllow: /'; 
 } else {
     content = 'User-agent: *\nDisallow: /';
 }
+
+console.log(`Generate robots.txt: ${ENVIRONMENT}`);
+console.log('Content for robots.txt:', content);
 
 fs.writeFileSync('static/robots.txt', content);
