d3ea25480088d6a53964875a492e8992c908ed07
Eron Powell <eron.powell@qrvey.com>
2024-12-07 00:30:25 -0600

    env stuff

--- v2-website/src/components/PopularArticles.js
+++ v2-website/src/components/PopularArticles.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import React, { Component } from "react";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 class PopularArticlesList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       popularArticles: [],
-      currentPageInfo: '' 
+      currentPageInfo: "",
     };
   }
 
@@ -18,42 +19,49 @@ class PopularArticlesList extends Component {
       this.interval = setInterval(this.checkURLChange, 1000);
 
       this.readyStateChangeListener = () => {
-        if (document.readyState === 'complete') {
+        if (document.readyState === "complete") {
           this.fetchPopularArticles();
         }
       };
-      document.addEventListener('readystatechange', this.readyStateChangeListener);
+      document.addEventListener(
+        "readystatechange",
+        this.readyStateChangeListener
+      );
     }
   }
 
   componentWillUnmount() {
     if (ExecutionEnvironment.canUseDOM) {
       clearInterval(this.interval);
-      document.removeEventListener('readystatechange', this.readyStateChangeListener);
+      document.removeEventListener(
+        "readystatechange",
+        this.readyStateChangeListener
+      );
     }
   }
-
 
   checkURLChange = () => {
     const currentURL = window.location.href;
     if (this.state.currentPageInfo !== currentURL) {
       this.setState({ currentPageInfo: currentURL });
-      if (window.location.pathname === '/' || window.location.pathname === '') {
+      if (window.location.pathname === "/" || window.location.pathname === "") {
         this.fetchPopularArticles();
       }
     }
   };
 
   setPopularArticlesInfo = () => {
-    if (document.readyState === 'complete') {
+    if (document.readyState === "complete") {
       this.fetchPopularArticles();
     }
   };
 
   fetchPopularArticles = async () => {
+    const apiKey = process.env.REACT_APP_X_API_KEY;
+
     const myHeaders = new Headers({
-      "x-api-key": "d41d8cd98f00b204e9800998ecf8427e",
-      "Content-Type": "application/json"
+      "x-api-key": "apiKey",
+      "Content-Type": "application/json",
     });
 
     const raw = JSON.stringify({
@@ -71,59 +79,90 @@ class PopularArticlesList extends Component {
                 order: [
                   {
                     sortDirection: "desc",
-                    summaryIndex: 0
-                  }
-                ]
+                    summaryIndex: 0,
+                  },
+                ],
               },
-              maxDataPoints: 50
-            }
+              maxDataPoints: 50,
+            },
           ],
           summaries: [
             {
               aggregate: "COUNT",
               questionid: "TPtOT4i5K",
-              type: "FORMULA"
-            }
-          ]
-        }
+              type: "FORMULA",
+            },
+          ],
+        },
       ],
       logic: [],
-      filters: []
+      filters: [],
     });
 
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: raw,
-      redirect: 'follow'
+      redirect: "follow",
     };
 
     try {
-      const response = await fetch("https://demo.qrvey.com/devapi/v4/user/VkRfd5iis/app/9b21hQU3D/qrvey/YwJoqX0Av/analytiq/uchart/results", requestOptions);
+      const response = await fetch(
+        "https://demo.qrvey.com/devapi/v4/user/VkRfd5iis/app/9b21hQU3D/qrvey/YwJoqX0Av/analytiq/uchart/results",
+        requestOptions
+      );
       const result = await response.json();
       const popularArticles = this.parseSummaryResult(result);
       this.setState({ popularArticles });
     } catch (error) {
-      console.log('error', error);
+      console.log("error", error);
     }
   };
 
   parseSummaryResult = (array) => {
-    return array[0].data.map(el => {
-      const splitter = el.key.split('@@');
-      const title = splitter[0] || '';
-      const url = splitter[1] || '';
-      const summary = el.summary[0];
-      return { title, url, summary };
-    }).filter(el => el.url && el.title !== 'Docs homepage');
+    return array[0].data
+      .map((el) => {
+        const splitter = el.key.split("@@");
+        const title = splitter[0] || "";
+        const url = splitter[1] || "";
+        const summary = el.summary[0];
+        return { title, url, summary };
+      })
+      .filter((el) => el.url && el.title !== "Docs homepage");
   };
 
   renderArticles = () => {
     return this.state.popularArticles.slice(0, 8).map((article, index) => (
-      <div key={index} className="popular-articles-list-item" style={{ display: 'flex', borderBottom: '1px solid silver', padding: '20px', paddingLeft: '0px', margin: '5px' }}>
-        <a className="popular-articles-list-link" style={{ color: '#FF5400' }} href={article.url.replace('/docs/docs', '/docs')}><b>{article.title}</b></a>
-        <div style={{ display: 'flex', alignItems: 'center', color: '#A1A1A1', marginLeft: '10px' }}>
-          <div className="highlights-icons icon-eye" style={{ marginRight: '5px' }}></div>
+      <div
+        key={index}
+        className="popular-articles-list-item"
+        style={{
+          display: "flex",
+          borderBottom: "1px solid silver",
+          padding: "20px",
+          paddingLeft: "0px",
+          margin: "5px",
+        }}
+      >
+        <a
+          className="popular-articles-list-link"
+          style={{ color: "#FF5400" }}
+          href={article.url.replace("/docs/docs", "/docs")}
+        >
+          <b>{article.title}</b>
+        </a>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "#A1A1A1",
+            marginLeft: "10px",
+          }}
+        >
+          <div
+            className="highlights-icons icon-eye"
+            style={{ marginRight: "5px" }}
+          ></div>
           <b>{article.summary} Views</b>
         </div>
       </div>
@@ -132,9 +171,7 @@ class PopularArticlesList extends Component {
 
   render() {
     return (
-      <div className="popular-articles-list-home">
-        {this.renderArticles()}
-      </div>
+      <div className="popular-articles-list-home">{this.renderArticles()}</div>
     );
   }
 }
